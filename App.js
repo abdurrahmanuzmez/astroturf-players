600cb553e5c7e2b013539b03b71a97955eb3ef28
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-08-20 23:49:43 +0300

    Not so much develop. Learnt some components from react native. Wait for me 2021

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import {Home} from './src/screens/home/Home'
+import {Home} from './src/screens/'
 
 export default function App() {
+  const image = { uri: "https://kidsupp.co/img/bg-img/welcome-bg.jpg" };
   return (
     <View style={styles.container}>
-      <Text>Hi there you asshole!</Text>
-      <StatusBar style="auto" />
-      <Home />
+      <ImageBackground source={image} style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+          <Home/>
+          <TouchableOpacity
+              onPress={() => alert('Hello, world!')}
+              style={{ backgroundColor: 'blue' }}>
+              <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+          </TouchableOpacity>
+
+          <StatusBar style="auto" />
+
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      flexDirection: "column",
   },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+    text: {
+        color: "grey",
+        fontSize: 30,
+        fontWeight: "bold"
+    }
+
 });
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,16 +1,27 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
-
-import {Header} from '../../components'
-
+import { Image,  StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import logo from '../../../assets/emrish.png';
 class Home extends React.Component{
     render() {
-        return(
+        return (
             <View>
-                <Header />
+                <Image source={logo} style={{ width: 305, height: 159 }} />
+                <Text style={{color: '#888', fontSize: 18}}>
+                    Start your adventure
+                </Text>
+
             </View>
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
 export default Home;
