0802e40a72c05646aa9750c7a72133c9ce04b11d
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-08-20 04:50:37 +0300

    my first react native project

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+class Header extends React.Component{
+    render() {
+        return(
+            <View>
+                <Text>header mq</Text>
+            </View>
+        )
+    }
+}
+
+export default Header;
