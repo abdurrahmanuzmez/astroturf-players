6445fabf3e58e1e8f67f5bf62525ec0c8e5d74bf
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-08-21 23:23:21 +0300

    Firebase works. (Auth)

--- App.js
+++ App.js
@@ -1,43 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {ImageBackground, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { SwitchNavigator } from 'react-navigation'
 
-import {Home} from './src/screens/'
+import {Home} from './src/screens/home'
+import SignUp from './src/screens/signup/SignUp'
+import Loading from './src/components/loading/Loading'
+import Login from './src/screens/login/Login'
 
-export default function App() {
-  const image = { uri: "https://kidsupp.co/img/bg-img/welcome-bg.jpg" };
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={image} style={styles.image}>
-        <Text style={styles.text}>Inside</Text>
-          <Home/>
-          <TouchableOpacity
-              onPress={() => alert('Hello, world!')}
-              style={{ backgroundColor: 'blue' }}>
-              <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-          </TouchableOpacity>
-
-          <StatusBar style="auto" />
-
-      </ImageBackground>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      flexDirection: "column",
-  },
-    image: {
-        flex: 1,
-        resizeMode: "cover",
-        justifyContent: "center"
+const App = SwitchNavigator(
+    {
+        Loading,
+        SignUp,
+        Login,
+        Home
     },
-    text: {
-        color: "grey",
-        fontSize: 30,
-        fontWeight: "bold"
+    {
+        initialRouteName: 'Loading'
     }
+)
+export default App
 
-});
